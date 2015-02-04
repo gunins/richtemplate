a940c3accbf8eeb65fdd1b77cd9ed06a4569a2d7
Guntars Simanskis <guntars@diamondgraphics.ie>
2015-02-04 12:00:17 +0000

    Fixed tests related to updated app

--- test/dev/templateTest.js
+++ test/dev/templateTest.js
@@ -15,12 +15,13 @@ define([
 
     describe('Templating Tests for dev source', function () {
 
-        describe('Checking Coder', function () {
+        describe('Checking if Coder', function () {
             it('first children should be a Style', function () {
                 var children = template.children[0],
                     templateId = template.templateId;
                 expect(children.data.style).to.equal('div.' + templateId +
-                                                     ' {\n  margin: 5px;\n  padding: 15px;\n  border: solid #008000 2px;\n}\n');
+                                                     ' {\n  margin: 5px;\n  padding: 15px;\n  border: solid #008000 2px;\n}\n.header.' +
+                                                     templateId + ' {\n  color: #008000;\n}\n');
             });
             it('Second children should be a Component', function () {
                 var children = template.children[1];
@@ -36,6 +37,7 @@ define([
                 expect(data.type).to.equal('pl');
                 expect(data.dataset.size).to.equal('34');
                 expect(data.dataset.item).to.equal('test item');
+
             });
         });
 
@@ -44,12 +46,14 @@ define([
                 var root = decoder._root,
                     templateId = root.templateId,
                     template = '<div class="' + templateId + '">\n    ' +
+                               '\n    <h2 class="'+templateId+' header">RootElement Header</h2>' +
                                '\n    <div id="e2" style="display:none"></div>\n    \n' +
                                '    <div id="e3" style="display:none"></div>\n</div>';
+
                 expect(root.template).to.equal(template);
                 expect(root.children.length).to.equal(4);
             });
-            it('Decoder Rendering items corectly', function () {
+            it('Decoder Rendering items correctly', function () {
                 var children = context.children,
                     footer = children.footer,
                     testthing = children.testthing;
@@ -63,8 +67,8 @@ define([
             it('if templateId is generated, and generated elements are attached to Node', function () {
                 var templateId = decoder._root.templateId,
                     els = Array.prototype.slice.call(el.querySelectorAll('.' + templateId));
-                expect(els.length).to.equal(2);
-                expect(els.indexOf(context.children.footer._node.el)).to.equal(1);
+                expect(els.length).to.equal(3);
+                expect(els.indexOf(context.children.footer._node.el)).to.equal(2);
             });
         });
     });
--- test/prod/templateTest.js
+++ test/prod/templateTest.js
@@ -21,7 +21,8 @@ define([
                 var children = template.children[0],
                     templateId = template.templateId;
                 expect(children.data.style).to.equal('div.' + templateId +
-                                                     ' {\n  margin: 5px;\n  padding: 15px;\n  border: solid #008000 2px;\n}\n');
+                                                     ' {\n  margin: 5px;\n  padding: 15px;\n  border: solid #008000 2px;\n}\n.header.' +
+                                                     templateId + ' {\n  color: #008000;\n}\n');
             });
             it('Second children should be a Component', function () {
                 var children = template.children[1];
@@ -46,8 +47,10 @@ define([
                 var root = decoder._root,
                     templateId = root.templateId,
                     template = '<div class="' + templateId + '">\n    ' +
-                               '\n    <div id="e2" style="display:none"></div>\n    \n' +
+                               '\n    <h2 class="'+templateId+' header">RootElement Header</h2>' +
+                                  '\n    <div id="e2" style="display:none"></div>\n    \n' +
                                '    <div id="e3" style="display:none"></div>\n</div>';
+
                 expect(root.template).to.equal(template);
                 expect(root.children.length).to.equal(4);
             });
@@ -65,8 +68,8 @@ define([
             it('if templateId is generated, and generated elements are attached to Node', function () {
                 var templateId = decoder._root.templateId,
                     els = Array.prototype.slice.call(el.querySelectorAll('.' + templateId));
-                expect(els.length).to.equal(2);
-                expect(els.indexOf(context.children.footer._node.el)).to.equal(1);
+                expect(els.length).to.equal(3);
+                expect(els.indexOf(context.children.footer._node.el)).to.equal(2);
             });
         });
     });
