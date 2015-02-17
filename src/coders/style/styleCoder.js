02429c60cd834c8f8b894fe2d8378fa3d591f99f
Guntars Simanskis <guntars@diamondgraphics.ie>
2015-02-17 21:05:01 +0000

    Added media and keyframes support in css

--- src/coders/style/styleCoder.js
+++ src/coders/style/styleCoder.js
@@ -10,7 +10,8 @@
     }
 }(this, function (Coder, less) {
     function applyId(content, id) {
-        var replacer = function (item) {
+        var classNames = getClassNames(content);
+        classNames.forEach(function (item) {
             if (item.trim().length > 0) {
                 var names = item.split(',');
                 names.forEach(function (name, index) {
@@ -31,14 +32,102 @@
                 });
                 content = content.replace(item, names.join(',')).trim() + '\n';
             }
-        }
-
-        var classNames = content.match(/[^}]*(?=)*(\{|$)/g);
-
-        classNames.forEach(replacer.bind(this));
+        });
         return content;
     }
 
+    function getClassNames(content) {
+        return content.match(/[^}]*(?=)*(\{|$)/g);
+    }
+
+    function getMedia(content) {
+        return content.match(/(@media[^{]+\{[\s\S]+?}\s*})/g);
+    }
+
+    function getCSSFromMedia(content) {
+        var regexp = /(?:@media[^{]+\{)([\s\S]+?})(?:\s*)}/g.exec(content);
+
+        return (regexp && regexp[1]) ? regexp[1] : regexp;
+    }
+
+    function getFrames(content) {
+        return content.match(/(@(-webkit-|-moz-)?keyframes[^{]+\{[\s\S]+?}\s*})/g);
+    }
+
+    function getframeName(content) {
+        return (/@(?:-webkit-|-moz-)?keyframes[^{](\w+)/g).exec(content)[1];
+    }
+
+    function removeItems(content, items) {
+        items.forEach(function (item) {
+            content = content.replace(item, '');
+        });
+        return content;
+    }
+
+    function parseMedia(media, id) {
+        media.forEach(function (item, index) {
+            var cssFromMedia = getCSSFromMedia(item);
+            media[index] = item.replace(cssFromMedia, '\n' + applyId(cssFromMedia, id));
+
+        });
+        return '\n' + media.join('\n');
+    }
+
+    function applyFrameNames(frameNames, content, id) {
+        frameNames.forEach(function (name) {
+            var match = content.match(new RegExp('(:(?:.+)?' + name + '(?:.+)?;)', 'g'));
+            if (match && match.length > 0) {
+                match.forEach(function (inner) {
+                    content = content.replace(inner, inner.replace(name, name + '_' + id));
+                })
+            }
+        });
+        return content;
+    }
+
+    function parseFrames(frames, id, frameNames) {
+        frames.forEach(function (frame, index) {
+            var frameName = getframeName(frame);
+            frames[index] = frame.replace(frameName, frameName + '_' + id);
+            if (frameNames.indexOf(frameName) === -1) {
+                frameNames.push(frameName);
+            }
+
+        });
+        return '\n' + frames.join('\n');
+    }
+
+    function parseCSS(content, id) {
+        var frameNames = [];
+        var parsedFrames;
+        var frames = getFrames(content);
+
+        if (frames && frames.length > 0) {
+            content = removeItems(content, frames);
+            parsedFrames = parseFrames(frames, id, frameNames);
+
+        }
+        var media = getMedia(content);
+        if (media && media.length > 0) {
+            content = removeItems(content, media);
+        }
+        content = applyId(content, id);
+
+        if (media && media.length > 0) {
+            content += parseMedia(media, id);
+        }
+
+        if (frames && frames.length > 0) {
+            content = applyFrameNames(frameNames, content, id) + parsedFrames;
+        }
+
+        return content;
+    }
+
+    // media queries /@media[^{]+\{([\s\S]+?})\s*}/g
+    // get frames   /(@(?:-webkit-|-moz-)?keyframes[^{]+\{[\s\S]+?}\s*})/g
+    //get frame name /@(?:-webkit-|-moz-)?keyframes[^{](\w+)/g
     var styleCoder = {
         tagName: 'style',
         noTag: true,
@@ -51,9 +140,9 @@
             /*    var currentUrl = '@current-url: "' + config['resources'] + '/' + name.substr(0, name.lastIndexOf('/')) +
              '";';
              var resourcesUrl = '@resources-url: "' + config['resources'] + '";';*/
-            less.render(content, function (e, output) {
-                data.style += applyId(output.css, templateId);
-            });
+            less.render(content, function(e, output){
+                data.style += parseCSS(output.css, templateId);
+            })
 
         }
     };
