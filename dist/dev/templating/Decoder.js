201b82ee1f927dc45bc1364fc6233f24e4e4e99f
Guntars Simanskis <guntars@diamondgraphics.ie>
2015-03-02 21:59:17 +0000

    Fix for children in safari

--- dist/dev/templating/Decoder.js
+++ dist/dev/templating/Decoder.js
@@ -160,15 +160,17 @@
         return context;
     };
     function runEls(children, fragment) {
-        Object.keys(children).forEach(function (key) {
-            if (children[key]._node.run !== undefined) {
-                children[key]._node.run.call(children[key], fragment);
-            }
-            if (children[key]._node.el === undefined && children[key]._node.template === undefined) {
-                children[key]._node.el = fragment.querySelector('#' + children[key]._node.id);
-                children[key]._node.el.removeAttribute('id');
-            }
-        });
+        if (children) {
+            Object.keys(children).forEach(function (key) {
+                if (children[key]._node.run !== undefined) {
+                    children[key]._node.run.call(children[key], fragment);
+                }
+                if (children[key]._node.el === undefined && children[key]._node.template === undefined) {
+                    children[key]._node.el = fragment.querySelector('#' + children[key]._node.id);
+                    children[key]._node.el.removeAttribute('id');
+                }
+            });
+        }
     }
 
     /**
